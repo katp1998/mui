c19c0df2e3d1270342ef000070f57fb5ba9544f0
Kethmie Perera <47343068+katp1998@users.noreply.github.com>
2023-01-09 18:05:07 +0530

    init
    Signed-off-by: Kethmie Perera <47343068+katp1998@users.noreply.github.com>

---
+++ pages/login.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {Box, Typography, TextField, Button} from '@mui/material';
+
+function login() {
+  return (
+    <>
+    <div>
+      <form >
+        <Box 
+        display = "flex" 
+        flexDirection={"column"} 
+        maxWidth = {400} 
+        alignItems="center" 
+        justifyContent={"center"} 
+        margin="auto" 
+        marginTop={5} 
+        padding={'50px'} 
+        borderRadius={5} 
+        boxShadow={'5px 5px 10px #ccc'}
+        sx={{":hover":{
+          boxShadow:'10px 10px 20px #ccc'
+        }}}>
+          <Typography 
+          variant='h3'
+          padding= {3}
+          textAlign = "center">LOGIN</Typography>
+          <TextField type='email' name="email"  required label="Email" variant="outlined" multiline placeholder='Enter email address' margin='normal' fullWidth color='error' />
+          <TextField type="password" name="password"  required label="Password" variant="outlined" multiline placeholder='Enter password' margin="normal" fullWidth color='error' id="outlined-password-input" />
+          <Button sx={{marginTop: 3}} variant ="contained"  type="submit" color="error" size="large" fullWidth>Login</Button>  
+          
+        </Box>
+
+        </form>
+    </div>
+    </>
+  )
+}
+
+export default login
